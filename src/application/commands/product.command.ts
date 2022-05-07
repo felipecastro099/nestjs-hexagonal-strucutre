ec5c6709f08ee1f4218e00a16cd34ba6bb8ede7f
Felipe Castro <me@felipecastro.dev>
2022-05-07 10:36:35 -0300

    add validation pipes

--- src/application/commands/product.command.ts
+++ src/application/commands/product.command.ts
@@ -1,7 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export default class ProductCommand {
+  @IsNotEmpty()
+  @IsString()
   public name: string;
 
+  @IsNotEmpty()
+  @IsString()
   public description: string;
 
+  @IsNotEmpty()
+  @IsNumber()
   public price: number;
 }
--- src/application/useCases/updateProduct.usecase.ts
+++ src/application/useCases/updateProduct.usecase.ts
@@ -2,17 +2,21 @@ import { Injectable, Inject } from '@nestjs/common';
 import Product from 'src/domain/product';
 import { ProductRepository } from 'src/domain/ports/product.repository';
 import { Optional } from 'typescript-optional';
+import ProductFactory from '../factory/product.factory';
+import ProductCommand from '../commands/product.command';
 
 @Injectable()
 export default class UpdateProductUseCase {
   constructor(
     @Inject('ProductRepository') private productRepository: ProductRepository,
+    private productFactory: ProductFactory,
   ) {}
 
   public async handler(
     productId: number,
-    product: Product,
+    productCommand: ProductCommand,
   ): Promise<Optional<Product>> {
+    const product = this.productFactory.createProduct(productCommand);
     return this.productRepository.updateProduct(productId, product);
   }
 }
